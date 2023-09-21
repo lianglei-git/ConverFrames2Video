597aeb1e743d30e1f798f99632fe5260619d0907
梁磊 <lei.liang@yizhun-ai.com>
2023-09-21 17:03:42 +0800

    what a max scale zoom for image of origin cnavs(png) Size!

--- frontend/src/canvasMock.ts
+++ frontend/src/canvasMock.ts
@@ -1,7 +1,10 @@
 import { $ } from "./utils";
+import {src} from './mock'
 
 const canvas = document.createElement('canvas');
 const context = canvas.getContext('2d');
+canvas.width = 720;
+canvas.height = 720;
 
 $('#root')[0].appendChild(canvas);
 let count = 0;
@@ -14,12 +17,17 @@ const getTextWH = (text: string): [number, number] => {
     return [0, 0]
 }
 
+const image = document.createElement('img');
+image.src = src;
+
 export const renderCanvasMock = () => {
     if (!context) return null;
     context?.clearRect(0, 0, canvas.width, canvas.height);
     if (count >= 100) {
         count = 0;
     }
+
+ 
     count++;
     context.fillStyle = '#000';
     context.fillRect(0, 0,canvas.width,canvas.height);
@@ -27,6 +35,9 @@ export const renderCanvasMock = () => {
     context.font = "48px serif";
     context.fillStyle='#fff'
     context.fillText('' + count, canvas.width / 2 - size.width, canvas.height / 2,)
+    if(count %2) {
+        context.drawImage(image, 0, 0, canvas.width, canvas.height)
+    }
     return canvas
 }
 
--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -6,12 +6,34 @@ let stderr = "";
 
 type MEMFStype = { name: string, data: ArrayBuffer }
 
-const createConverFrames = () => {
+const createConverFrames = (maxWidth = 1080, maxHeight = 1080) => {
     let Ok: (buffers: MEMFStype[]) => any
     let c = 0;
+    let bufferSizeMB = 0;
     let frames: MEMFStype[] = []
+    const targetCanvas = renderCanvasMock();
+
+    if (targetCanvas) {
+        /** 比例最大限度缩放 */
+        const cWidth = targetCanvas.width;
+        const cHeight = targetCanvas.height;
+        var scale = Math.min(maxWidth / cWidth, maxHeight / cHeight);
+        var width = ~~(cWidth * scale);
+        var height = ~~(cHeight * scale);
+        if (width % 2) {
+            width += 1;
+        }
+        if (height % 2) {
+            height += 1;
+        }
+        targetCanvas.width = width;
+        targetCanvas.height = height;
+    }
+
     let time = setInterval(() => {
         const targetCanvas = renderCanvasMock()
+        if (!targetCanvas) return void 0;
+
         c++;
         if (c == 20) {
             clearInterval(time);
@@ -19,25 +41,70 @@ const createConverFrames = () => {
         }
         targetCanvas?.toBlob(async (v) => {
             const buffer = await v?.arrayBuffer();
+
             if (buffer) {
+                bufferSizeMB += buffer.byteLength / (1024 * 1024);
+                console.log('createConverFrames ~~ bufferSizeMB -->', bufferSizeMB);
                 frames.push({
                     name: `${c}.png`,
                     data: buffer
                 })
             }
         })
-    }, 100)
+    }, 300)
 
     return new Promise(res => (Ok = res))
 }
 
+
+const downloadMp4 = (data, fileName) => {
+    const src = URL.createObjectURL(
+        new Blob([data], {
+            type: 'video/mp4'
+        })
+    );
+    const link = document.createElement('a');
+    link.href = src;
+    link.setAttribute('download', `${fileName}.mp4`);
+    link.click();
+    document.body.appendChild(link);
+    link.remove();
+};
 
 
 
 const imgToVideo = (frames) => {
     const time = '20000ms';
     const result = (FFmpeg as any)({
-        arguments: ['-f', 'image2', '-framerate', '1', '-i', '%d.png', "-r", "25", "-pix_fmt", "yuv420p", "-preset", "ultrafast", '-t', "60", "-y", 'video.mp4'],
+        arguments: [
+            '-f',
+            'image2',
+            '-framerate',
+            // 0.33秒每张图片
+            '3', // 1 / 0.33
+            '-i',
+            '%d.png',
+            // '-vcodec',
+            // 'libx264',
+            //   '-r',
+            //   '24',
+            '-pix_fmt',
+            'yuv420p',
+            '-preset',
+            'ultrafast',
+            // '-s',
+            // 'INITIAL_MEMORY=4146435072',
+            //   '-t',
+            //   '18s',
+            //   '-bufsize',
+            //   '99M',
+            //   '-t',
+            //   '18',
+            // '-vb',
+            // '200M',
+            '-y',
+            'video.mp4'
+        ],
         MEMFS: frames,
         print: function (data) { stdout += data + "\n"; },
         printErr: function (data) { stderr += data + "\n"; },
@@ -48,18 +115,12 @@ const imgToVideo = (frames) => {
         },
     })
     console.log(result);
-    const videoEl = $("#video")[0] as HTMLVideoElement;
-    videoEl.src = URL.createObjectURL(new Blob([result.MEMFS[0].data], {
-        type: 'video/mp4'
-    }))
-    const link = document.createElement('a')
-    link.href = videoEl.src
-    link.setAttribute('download', `${new Date().getTime()}.mp4`)
-    link.click()
-    document.body.appendChild(link)
+    downloadMp4(result.MEMFS[0].data, 'frame2video')
 
 }
 
 
-createConverFrames()
+
+
+$('#create')[0].onclick = () => createConverFrames()
     .then(imgToVideo)
